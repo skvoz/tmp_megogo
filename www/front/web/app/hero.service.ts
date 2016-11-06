6453b62685e6a4d626f1dd5929d1dd5b500f8901
git <bulavaelena@mail.ru>
2016-11-06 23:45:01 +0200

    decouple view , and components, add routing, change view heroes detail part

---
+++ www/front/web/app/hero.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+@Injectable()
+export class HeroService {
+    getHeroesSlowly(): Promise<Hero[]> {
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(resolve, 2000)) // delay 2 seconds
+            .then(() => this.getHeroes());
+    }
+
+    getHeroes(): Promise<Hero[]> {
+        return Promise.resolve(HEROES);
+    }
+
+    getHero(id: number): Promise<Hero> {
+        return this.getHeroes()
+            .then(heroes => heroes.find(hero => hero.id === id));
+    }
+}
